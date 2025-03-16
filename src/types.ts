920cf9df4d618617bad99a0135c14c2c692eb491
AndreyKuklinov <37548463+AndreyKuklinov@users.noreply.github.com>
2025-03-16 15:24:48 +0500

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,39 @@
+export type UserType = 'usual' | 'pro';
+
+export interface User {
+  name: string;
+  email: string;
+  avatarPath?: string;
+  password: string;
+  userType: UserType;
+}
+
+export interface Offer {
+  title: string;
+  description: string;
+  postedDate: Date;
+  city: string;
+  previewImage: string;
+  images: string[];
+  isPremium: boolean;
+  isFavorite: boolean;
+  rating: number;
+  type: 'apartment' | 'house' | 'room' | 'hotel';
+  roomsCount: number;
+  guestsCount: number;
+  price: number;
+  amenities: string[];
+  author: User;
+  commentsCount: number;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
+export interface Comment {
+  text: string;
+  createdAt: Date;
+  rating: number;
+  user: User;
+}
