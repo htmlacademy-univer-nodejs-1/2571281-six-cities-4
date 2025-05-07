50c35928e1a23fe18756b2560edb387b6723f366
AndreyKuklinov <37548463+AndreyKuklinov@users.noreply.github.com>
2025-05-07 14:58:21 +0500

    Pretty env log

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,7 +1,9 @@
 import { logger } from '../libs/logger.js';
+import { appConfig } from '../config/config.js';
 
 export class Application {
   public async init(): Promise<void> {
-    logger.info('Application initialised');
+    const port = appConfig.get('PORT');
+    logger.info(`Application initialised. Configured port: ${port}`);
   }
 }
---
+++ src/config/config.ts
@@ -0,0 +1,30 @@
+import { config as loadDotenv } from 'dotenv';
+import convict from 'convict';
+import convictFormatWithValidator from 'convict-format-with-validator';
+
+loadDotenv();
+
+convict.addFormats(convictFormatWithValidator);
+
+export const appConfig = convict({
+  PORT: {
+    doc: 'Port on which the HTTP server will listen',
+    format: 'port',
+    default: 3000,
+    env: 'PORT'
+  },
+  DB_HOST: {
+    doc: 'MongoDB host IP address',
+    format: 'ipaddress',
+    default: '127.0.0.1',
+    env: 'DB_HOST'
+  },
+  SALT: {
+    doc: 'Random salt for hashing operations',
+    format: String,
+    default: '',
+    env: 'SALT'
+  }
+});
+
+appConfig.validate({ allowed: 'strict' });
