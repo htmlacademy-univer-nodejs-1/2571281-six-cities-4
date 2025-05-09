1ed96fa1ae4844f6849a181f836ec31fe38ce148
AndreyKuklinov <37548463+AndreyKuklinov@users.noreply.github.com>
2025-05-09 23:51:33 +0500

    Implement register user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,23 @@
 import { Request, Response } from 'express';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 
 import { Controller } from '../../common/controller/controller.js';
 import { HttpMethod } from '../../common/http-method.enum.js';
+import { CreateUserDto } from './create-user.dto.js';
+import { createSHA256 } from '../../libs/crypto.js';
+import { UserService } from './user.service.js';
+import { TYPES } from '../../types.js';
+import { UserResponseDto } from './user-response.dto.js';
+import { ConfigService } from '../../config/config.service.js';
+import { RegisterUserDto } from './register-user.dto.js';
+import { plainToInstance } from 'class-transformer';
 
 @injectable()
 export class UserController extends Controller {
-  constructor() {
+  constructor(
+    @inject(TYPES.UserService) private readonly userService: UserService,
+    @inject(TYPES.Config) private readonly config: ConfigService,
+  ) {
     super();
 
     this.addRoute({
@@ -28,9 +39,24 @@ export class UserController extends Controller {
     });
   }
 
-  private async register(_: Request, res: Response): Promise<void> {
-    this.notImplemented(res);
+  private async register(
+    { body }: Request<unknown, unknown, RegisterUserDto>,
+    res: Response
+  ): Promise<void> {
+
+    const salt = this.config.get('SALT');
+    const passwordHash = createSHA256(body.password, salt);
+
+    const createDto: CreateUserDto = {
+      ...body,
+      passwordHash,
+    };
+
+    const newUser = await this.userService.create(createDto);
+
+    this.created(res, plainToInstance(UserResponseDto, newUser));
   }
+
 
   private async login(_: Request, res: Response): Promise<void> {
     this.notImplemented(res);
