8fc998717879782c3c9fc71de572250755575d5f
AndreyKuklinov <37548463+AndreyKuklinov@users.noreply.github.com>
2025-05-11 08:33:32 +0500

    Update comment.controller.ts

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -5,6 +5,7 @@ import { HttpMethod } from '../../common/http-method.enum.js';
 import { TYPES } from '../../types.js';
 import { CommentServiceInterface } from './comment.service.interface.js';
 import { CreateCommentDto } from './create-comment.dto.js';
+import { HttpError } from '../../common/errors/http-error.js';
 
 @injectable()
 export class CommentController extends Controller {
@@ -32,14 +33,23 @@ export class CommentController extends Controller {
   private async index(req: Request, res: Response): Promise<void> {
     const { offerId } = req.params;
     const comments = await this.commentService.findByOffer(offerId);
+
+    if (!comments) {
+      throw new HttpError(404, 'Offer not found', { offerId });
+    }
+
     this.ok(res, comments);
   }
 
   private async create(req: Request, res: Response): Promise<void> {
     const { offerId, userId } = req.params;
     const dto = req.body as CreateCommentDto;
-
     const comment = await this.commentService.create(offerId, userId, dto);
+
+    if (!comment) {
+      throw new HttpError(404, 'Offer not found', { offerId });
+    }
+
     this.created(res, comment);
   }
 }
